6ee874f942562cda867f7cfdab32224fa664b007
Padshiy <intgena@gmail.com>
2025-07-14 19:50:52 +0300

    добавлены заголовки

--- SFMLformater.cpp
+++ SFMLformater.cpp
@@ -1,12 +1,27 @@
 ﻿#include <SFML/Graphics.hpp>
-
+#include<iostream>
+using namespace std;
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    sf::CircleShape circle(220.f);
-    circle.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML window");
+    const std::string pathFront = "C:\Windows\Fonts\Arial\arialbd.ttf";
+    sf::Font font;
+    sf::Text header;
+    if (!font.loadFromFile("./fonts/arialbd.ttf"))
+    {
+        new exception ("не найден формат");
+    }
+    header.setFont(font);
+    header.setString("hfsldhsh");
+    header.setCharacterSize(24);
+    
+    sf::Text header2;
+    header2.setFont(font);
+    header2.setString("Hello World"); header2.setCharacterSize(18);
+    header2.setPosition(sf::Vector2f(0, 30));
 
+        
 
     while (window.isOpen())
     {
@@ -16,8 +31,8 @@ int main()
                 window.close();
 
         window.clear();
-        window.draw(circle);
-
+        window.draw(header);
+        window.draw(header2);
         window.display();
     }
 }
